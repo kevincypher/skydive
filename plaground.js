aac26cc8f14720e55b652e3010f98f35cd6dc771
kevincypher <majombombombile@gmail.com>
2024-03-12 04:18:59 +0400

    added javascript only for practice not for the yet but will be added soon

---
+++ plaground.js
@@ -0,0 +1,89 @@
+// do what you like
+
+// let steve = 32;
+
+// steve = 35;
+
+// console.log(steve);
+
+// alert("hello, w0rld")
+
+// let firstName = 'Baby';
+// let secondName = 'Boy';
+// let fullName = firstName + ' ' + secondName;
+
+// console.log(fullName[3]);
+
+// console.log(fullName.length);
+
+//let result = fullName.toUpperCase();
+
+// console.log(fullName.indexOf('a'));
+
+//result = result.toLowerCase();
+
+// let result = fullName.slice(5,8); 
+
+//let result = fullName.substr(1,5);
+
+// let result = fullName.lastIndexOf('y');
+
+
+
+// console.log(result);
+
+// let radius = 10;
+// let pi = 3.14;
+
+// let area = pi * radius**2 ;
+// console.log(area);
+//console.log(radius/2);
+
+// // radius++;
+//  radius--;
+
+
+//short hand form of operations
+// radius += 10;
+//radius -= 9;
+// radius *= 3;
+
+//b o d m a s also applicable in js mathematical operations
+//nan = not a number  
+// console.log(radius);
+
+//concatenation 
+// const title = 'Cypher got announced';
+// const prize = 'BET award';
+// const times = '30th';
+
+//previously learned + concatenation
+//template string method
+// let result = `${title} the winner of the ${prize} for the ${times} time in a row`;
+// console.log(result);
+
+// for(let i = 0; i < 5; i++){
+//     console.log(`in loop`, i);
+// }
+// console.log(`loop finished`);
+
+// const names = ['SpongeBob', 'SquidWard', 'Patrick'];
+
+// for(let i = 0; i < names.length; i++){
+//     // console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`
+//     console.log(html);
+// }
+
+//while loop
+//   let i = 0;
+//   while(i < 5){
+//     console.log(i);
+//     i++;
+//   }
+
+// let i = 0;
+// while(i < names.length ){
+//     console.log(names[i]);
+//     i++;
+// }
